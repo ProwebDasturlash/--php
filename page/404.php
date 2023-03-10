6756c6403b80e7e2ea0e1decd2dbcf6ff5e1b89d
Ismoil <ismoil@gmail.com>
2023-03-10 20:14:13 +0500

    first commit

---
+++ page/404.php
@@ -0,0 +1,6 @@
+
+        <main class="main">
+            <section class="head">
+                <h2 class="head__title">К сожалению, страница не найдена :(</h2>
+            </section>
+        </main>
---
+++ page/contact.php
@@ -0,0 +1,12 @@
+
+        <main class="main">
+            <section class="head">
+                <h2 class="head__title">Контакты</h2>
+                <p class="head__date">Сегодня 10 Март 2023 год</p>
+            </section>
+            <secrion class="body">
+                <h2 class="body__title">Адрес</h2>
+                <p class="body__desc">Узбекистан г.Ташкент</p>
+            </secrion>
+            <iframe class="map" src="https://maps.app.goo.gl/Fd26sWMwZxjJwHB57" allowfullscreen=""></iframe>
+        </main>
